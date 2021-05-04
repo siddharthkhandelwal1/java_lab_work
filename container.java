907d4099a0b404c44d2e989d2324ef791719d2f7
Siddharth <69121694+siddharthkhandelwal1@users.noreply.github.com>
2021-05-04 15:37:38 +0530

    Create container.java

---
+++ container.java
@@ -0,0 +1,26 @@
+  
+import java.awt.*;
+
+
+public class Container  {
+
+        public static void main(String[] args){
+
+            Frame f1 = new Frame();
+            Checkbox p1 = new Checkbox(" java");
+            TextField y1 = new TextField(10);
+            Choice r = new Choice();
+            r.setBounds(100, 100, 75, 75);
+            r.add("Apple");
+            r.add("Ball");
+            r.add("pet");
+            r.add("dog");
+            f1.add(r);
+            f1.add(p1);
+            f1.add(y1);
+
+            f1.setVisible(true);
+            f1.setSize(500, 500);
+            f1.setLayout(new FlowLayout());
+        }
+    }             
